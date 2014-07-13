a9d944470f318dc7802a9c75ded6c039aef43025
Abhijit Shetti <ashetti@gmail.com>
2014-07-13 01:15:35 -0700

    All other R and png files
    All other R and png files

---
+++ plot3.R
@@ -0,0 +1,15 @@
+tab5rows <- read.table("household_power_consumption.txt", header = TRUE, nrows = 5,sep = ";")
+classes <- sapply(tab5rows,class)
+classes[1]="character"
+classes[2]="character"
+tabAll <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", nrows = 70000, na.strings = "?",colClasses=classes)
+dataset <- rbind(subset(tabAll,Date=="1/2/2007"),subset(tabAll,Date=="2/2/2007"))
+dataset$Date=as.Date(dataset$Date,format = "%d/%m/%Y")
+dataset$DT<-paste(dataset$Date,dataset$Time)
+dataset$DateTime <- strptime(dataset$DT,format="%Y-%m-%d %H:%M:%S")
+png(filename="plot3.png",width=480,height=480)
+plot(dataset$DateTime,as.numeric(dataset2$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering")
+lines(dataset$DateTime,as.numeric(dataset2$Sub_metering_2),col="red")
+lines(dataset$DateTime,as.numeric(dataset2$Sub_metering_3),col="blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=c(1,1,1))
+dev.off()
